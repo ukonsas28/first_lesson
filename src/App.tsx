392734bc788ddfade14dd69d01f6a5e76d879f57
Alexey Shirokov <shirokovam@lad24.ru>
2021-03-13 15:09:30 +0300

    lesson 2 clear app

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,4 @@
 import React from "react";
-import "./App.css";
-import CustomCounter from "./CustomCounter";
 
 function App() {
   // eslint-disable-next-line no-console
@@ -8,9 +6,7 @@ function App() {
 
   return (
     <>
-      <CustomCounter title="Яблоко" limit={13} />
-      <CustomCounter title="Апельсин" limit={8} />
-      <CustomCounter title="Груша" limit={25} />
+      <div> Hello</div>
     </>
   );
 }
